4c9d007e7034480c6b882c5939c36fc8ce725d78
jeet-begstep <118178882+jeet-begstep@users.noreply.github.com>
2023-08-18 14:25:39 +0530

    rtm chat sent masseg issue fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import chatIcon from './chat2.png'
-import React, { useState, StyleSheet, useContext } from 'react';
-import AgoraUIKit, { PropsContext, popUpStateEnum, rtmStatusEnum } from 'agora-react-uikit';
+import React, { useState, StyleSheet, useContext, useRef, useEffect } from 'react';
+import AgoraUIKit, { PropsContext, RtcContext, popUpStateEnum, rtmStatusEnum } from 'agora-react-uikit';
 import {
   AgoraVideoPlayer,
   createClient,
@@ -34,21 +34,199 @@ const useClient = createLazyClient()
 
 const App = () => {
 
-
-
-  const [messageList, setMessageList] = useState([{id:235324,message:"sdfsdkf"}])
-
-
-
-
-  const [videoCall, setVideoCall] = useState(true);
-  const [isChatEnabel,setChatEnabel]=useState(false)
-
-  const rtcProps = {
+  const rtcPropst = {
     appId: '1e07c1131da141dd8734a2fa01e82955',
     channel: 'test', // your agora channel
     //token: null //'<Your channel Token>' use null or skip if using app in testing mode
   };
+  const {rtcProps, rtmProps} = useContext(PropsContext);
+const [messageList, setMessageList] = useState([])
+const timerValueRef = useRef(5)
+const localUid = useRef('')
+const rtmClient = useClient(rtcPropst.appId)
+const channel = useChannel(rtmClient, rtcPropst.channel)
+const {
+  localUid:rtcUid,
+  localAudioTrack,
+  localVideoTrack,
+  dispatch,
+  channelJoined
+} = useContext(RtcContext)
+
+
+const login = async () => {
+  console.log("login")
+
+  const { tokenUrl } = rtcProps
+  if (tokenUrl) {
+    try {
+      const res = await fetch(
+        tokenUrl + '/rtm/' + (rtmProps?.uid || String(localUid.current))
+      )
+      const data = await res.json()
+      const serverToken = data.rtmToken
+      await rtmClient.login({
+        uid: rtmProps?.uid || String(localUid.current),
+        token: serverToken
+      })
+      console.log("login S")
+
+      timerValueRef.current = 5
+    } catch (error) {
+      setTimeout(async () => {
+        timerValueRef.current = timerValueRef.current + timerValueRef.current
+        login()
+      }, timerValueRef.current * 1000)
+    }
+  } else {
+    try {
+      await rtmClient.login({
+        uid: rtmProps?.uid || String(localUid.current),
+        token: rtmProps?.token || undefined
+      })
+      timerValueRef.current = 5
+      console.log("login S")
+
+    } catch (error) {
+      setTimeout(async () => {
+        timerValueRef.current = timerValueRef.current + timerValueRef.current
+        login()
+      }, timerValueRef.current * 1000)
+    }
+  }
+}
+
+const joinChannel = async () => {
+  try {
+    await channel.join()
+    console.log("joinChannel")
+    timerValueRef.current = 5
+  } catch (error) {
+    setTimeout(async () => {
+      timerValueRef.current = timerValueRef.current + timerValueRef.current
+      joinChannel()
+    }, timerValueRef.current * 1000)
+  }
+}
+
+const init = async () => {
+  //setRtmStatus(rtmStatusEnum.initialising)
+  // rtmProps?.uid
+  //   ? (localUid.current = String(rtmProps.uid))
+  //   :
+  console.log("init agora",rtcProps.uid,rtcProps.channel, String(rtcProps.uid),localUid.current)
+  rtcProps.uid
+    ? (localUid.current = String(rtcProps.uid))
+    : (localUid.current = String(timeNow()))
+
+  rtmClient.on('ConnectionStateChanged', (state, reason) => {
+    console.log(state, reason)
+  })
+
+  rtmClient.on('TokenExpired', async () => {
+    const { tokenUrl } = rtcProps
+    console.log('token expired - renewing')
+    if (tokenUrl) {
+      try {
+        const res = await fetch(
+          tokenUrl + '/rtm/' + (rtmProps?.uid || localUid.current)
+        )
+        const data = await res.json()
+        const serverToken = data.rtmToken
+        await rtmClient.renewToken(serverToken)
+        timerValueRef.current = 5
+      } catch (error) {
+        console.error('TokenExpiredError', error)
+      }
+    }
+  })
+
+  rtmClient.on('MessageFromPeer', (message, peerId) => {
+    //handleReceivedMessage(message, peerId)
+  })
+
+  channel.on('ChannelMessage', (message, peerId) => {
+    //handleReceivedMessage(message, peerId)
+  })
+
+  channel.on('MemberJoined', async (peerId) => {
+   // await sendPeerMessage(createUserData(), peerId)
+  })
+  channel.on('MemberCountUpdated', async (count) => {
+    console.log('RTM-MemberCountUpdated: ', count)
+  })
+
+  // handle RTM callbacks
+  // if (rtmCallbacks?.channel) {
+  //   Object.keys(rtmCallbacks.channel).map((callback) => {
+  //     if (rtmCallbacks.channel) {
+  //       channel.on(
+  //         callback as keyof RtmEvents.RtmChannelEvents,
+  //         rtmCallbacks.channel[callback]
+  //       )
+  //     }
+  //   })
+  // } else if (rtmCallbacks?.client) {
+  //   Object.keys(rtmCallbacks.client).map((callback) => {
+  //     if (rtmCallbacks.client) {
+  //       rtmClient.on(
+  //         callback as keyof RtmEvents.RtmClientEvents,
+  //         rtmCallbacks.client[callback]
+  //       )
+  //     }
+  //   })
+  // }
+
+  // if (rtcProps.tokenUrl) {
+  //   const { tokenUrl, uid } = rtcProps
+  //   rtmClient.on('TokenExpired', async () => {
+  //     console.log('token expired')
+  //     const res = await fetch(tokenUrl + '/rtm/' + (uid || 0) + '/')
+  //     const data = await res.json()
+  //     const token = data.rtmToken
+  //     rtmClient.renewToken(token)
+  //   })
+  // }
+
+  // setRtmStatus(rtmStatusEnum.loggingIn)
+   await login()
+  // setRtmStatus(rtmStatusEnum.loggedIn)
+   await joinChannel()
+  // setRtmStatus(rtmStatusEnum.connected)
+  // // console.log('!rtcUid', rtcUid, channelJoined)
+  // setUsernames((p) => {
+  //   return { ...p, 0: rtmProps?.username }
+  // })
+  // sendChannelMessage(createUserData())
+}
+
+
+
+useEffect(() => {
+  const setupRtm = async () => {
+    console.log("USEEFFCAT Call",rtcProps.channel,"rtcProps.appID",rtcProps.channel)
+    await init();
+    };
+  setupRtm();
+  return () => {
+   // end();
+  };
+  // if (channelJoined) {
+  //   init()
+  //  // setLoggedIn(true)
+  // }
+  // return () => {
+  //   if (channelJoined) {
+  //    // end()
+  //   }
+  // }
+
+ }, [rtcProps.channel,rtcProps.appId,rtcProps.channelJoined]);
+
+  const [videoCall, setVideoCall] = useState(true);
+  const [isChatEnabel,setChatEnabel]=useState(false)
+
+
   const callbacks = {
     EndCall: () => setVideoCall(false),
   };
@@ -115,6 +293,30 @@ const App = () => {
 
     }
   }
+
+  const sendChannelMessage = async (payload) => {
+    const text={message:payload,id:(new Date()).getTime()}
+
+    const message = rtmClient.createMessage({
+      text: JSON.stringify(text),
+      messageType: AgoraRTM.MessageType.TEXT
+    })
+    try {
+      await channel.sendMessage(message)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  const sentMessage=(message)=>{
+    const text={message:message,id:(new Date()).getTime()}
+    const rtmMessage={
+     
+      text:JSON.stringify(text),
+      messageType:112,
+    }
+    
+    //if(props.sentMessage) props.sentMessage(message)
+   }
   const handChatEnabel=()=>{
     setChatEnabel(!isChatEnabel)
   }
@@ -123,15 +325,18 @@ const App = () => {
       chatUi=<div style={{
         flex: 1,
     position: 'absolute',
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
     //  left: 0,
     //  top: 0,
+    
     right: 0, bottom: 70,
     opacity: 0.5,
     width:350,
     marginRight:40,
     backgroundColor: 'black',
     } }>
-       <Goals goalList={messageList} ></Goals>
+       <Goals sentMessage={sendChannelMessage} goalList={messageList} ></Goals>
        </div>
   }else{
     chatUi=null
@@ -142,7 +347,7 @@ const App = () => {
   return videoCall ? (
     <div>
       <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
-        <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} rtmCallbacks={rtmCallback} />
+        <AgoraUIKit rtcProps={rtcPropst} callbacks={callbacks} rtmCallbacks={rtmCallback} />
         {chatUi}
       </div>
       <div  style={{position:"absolute",bottom:0,right:"10%" ,zIndex:30}}>
--- src/goallist/Goal-list.js
+++ src/goallist/Goal-list.js
@@ -1,37 +1,52 @@
-  import React from "react";
-  import './goal-list.css';
+import React, { useState } from "react";
+import './goal-list.css';
 import imageProfile from '../user_profile.png'
 import icSent from '../ic_sent.png'
 
-  const Goals=(goalList)=>{
-    console.log(goalList)
-    return  <div>
-      <ul className="goal-list">
-    {
-  
-  goalList.goalList.map((item) => {
-    return (<div style={{display:"flex",flexDirection:"row", margin:16}} >
-        <img src={imageProfile} style={{width:32,height:32}}></img>
-        <span style={{marginInlineStart:16,color:"white"}}>{item.name+":  "+item.message}</span>
-    </div>)
-})
-         // goalList.goalList.map((goal)=>{
+const Goals = (porps) => {
+  console.log(porps)
+
+  const onSent = () => {
+    console.log(inputTextValue)
+    porps.sentMessage(inputTextValue)
+    onReset()
+  }
+  const onReset = () => {
+    setInputText('');
+  }
+  const [inputTextValue, setInputText] = useState("")
+  const textChangeHandler = (value) => {
+    setInputText(value)
+  }
+  return <div>
+    <ul className="goal-list">
+      {
+        //i was working on some urgent issue now i am looking into this 
+        porps.goalList.map((item) => {
+          return (<li style={{ height: '10px', border: '1px solid black',paddingBlockStart:0,paddingInlineStart:0 }}>
+            <div style={{ display: "flex", flexDirection: "row", }} >
+              <img src={imageProfile} style={{ width: 32, height: 32, }}></img>
+              <span style={{ marginInlineStart: 16, color: "white" }}>{item.name + ":  " + item.message}</span>
+            </div>
+          </li>)
+        })
+        // goalList.goalList.map((goal)=>{
         //   return  <li key={goal.id}>{goal.message}</li>
         // })
       }
-  </ul>
-  
-  // <div style={{display:"flex", flexDirection: "row", justifyContent: "flex-start" }}>
-  <input style={{height:30 ,width:"70%", marginInlineStart:20,marginRight:8}}  />
-  {/* <Button title="slkdf"><Image src="../assets/images/chat.png"></Image></Button> */}
-  <div style={{justifyContent:"center",marginBottom:8,}} onPress={() => {  }}>
-      <div>
-          <img    src={icSent}/>
+    </ul>
+
+  // <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
+      <input style={{ height: 30, width: "70%", marginInlineStart: 20, marginRight: 8 }} value={inputTextValue} onChange={e => textChangeHandler(e.target.value)} />
+      {/* <Button title="slkdf"><Image src="../assets/images/chat.png"></Image></Button> */}
+      <div style={{ justifyContent: "center", marginBottom: 8, }} onClick={onSent}>
+        <div>
+          <img src={icSent} />
+        </div>
       </div>
-  </div>
-</div>
     </div>
+  </div>
 
 
-  }
-  export default Goals;
+}
+export default Goals;
